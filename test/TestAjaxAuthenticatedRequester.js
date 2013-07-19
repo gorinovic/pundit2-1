c6abd99c3bffb0ac6988cfa3d7a74a9a3b6b92e9
Simone Fonda <fonda@netseven.it>
2013-07-19 21:01:14 +0200

    New test for Ajax Authenticated Requester, loginStatus = 0

--- test/TestAjaxAuthenticatedRequester.js
+++ test/TestAjaxAuthenticatedRequester.js
@@ -3,27 +3,15 @@ define([
     "pundit/AuthenticatedRequester"
 ], function(query, AuthenticatedRequester) {
 
-    /*
-    TODO: worth writing tests with fakeServer?
-    
-    suite("AJAX AR (sinon server)", function() {
-        var ar, server;
-            
-        setup(function() {
-            delete _PUNDIT;
-            ar = new AuthenticatedRequester({});
-            ar.placeAt(query('body')[0]);
-            server = sinon.fakeServer.create();
-        });
-        
-        teardown(function() {
-            ar.destroy();
-            delete ar;
-            delete _PUNDIT;
-        });
-        
-        test('LoginStatus = 1 from server', function(done) {
-            var res = {
+    suite("AJAX Authenticated Requester", function() {
+        var ar, 
+            xhr, 
+            requests,
+            resLogin0 = {
+                "loginStatus": 0,
+                "loginServer": "http://foo.foo.bar/login.jsp"
+            },
+            resLogin1 = {
                 loginStatus: 1,
                 id: "b36f77b4",
                 uri: "http://swickynotes.org/notebook/resource/12345678",
@@ -33,39 +21,22 @@ define([
                 fullName: "Fake User",
                 email: "fake.user@gmail.com",
                 loginServer: "http://fake.loginserver.foo/annotationserver/login.jsp"
-            },
-            callback = sinon.spy();
+            };
             
-            server.respondWith('OPTIONS', _PUNDIT.ns.asUsersCurrent, [200,{}, '']);
-            server.respondWith('GET', _PUNDIT.ns.asUsersCurrent, [200, { "Content-Type": "application/json" }, JSON.stringify(res)]);
-            
-            ar.isLoggedIn(function(a, b) {
-                callback(a, b);
-                sinon.assert.calledWith(callback, true, res);
-                done();
-            });
-
-            server.respond();
-        });
-        
-    });
-    */
-
-    suite("AJAX Authenticated Requester [Sinon.JS]", function() {
-        var ar, xhr, request;
+        // DEBUG: overwrite the global XMLHttpRequest with sinon's, since 
+        // it does not under node :(
+        // Dont think browser's tests will ever notice this, otherwise
+        // wrap in some if typeof global or something
+        xhr = sinon.useFakeXMLHttpRequest();
+        XMLHttpRequest = sinon.FakeXMLHttpRequest;
+        xhr.onCreate = function(xhr) { requests.push(xhr); };
 
         setup(function() {
             delete _PUNDIT;
-
-            xhr = sinon.useFakeXMLHttpRequest();
-            // DEBUG: overwrite the global XMLHttpRequest with sinon's, since 
-            // it does not under node :(
-            XMLHttpRequest = sinon.FakeXMLHttpRequest;
+            ar = new AuthenticatedRequester({});
+            ar.placeAt(query('body')[0]);
+            ar.startup();
             requests = [];
-            xhr.onCreate = function(xhr) { requests.push(xhr); };
-            
-            ar = new AuthenticatedRequester({});
-            ar.placeAt(query('body')[0]);
         });
         
         teardown(function() {
@@ -75,26 +46,27 @@ define([
         });
         
         test('LoginStatus = 1 from server', function(done) {
-            var res = {
-                loginStatus: 1,
-                id: "b36f77b4",
-                uri: "http://swickynotes.org/notebook/resource/12345678",
-                openid: "https://www.google.com/accounts/o8/id?id=1234567890abcdef",
-                firstName: "Fake",
-                lastName: "User",
-                fullName: "Fake User",
-                email: "fake.user@gmail.com",
-                loginServer: "http://fake.loginserver.foo/annotationserver/login.jsp"
-            },
-            callback = sinon.spy();
+            var callback = sinon.spy();
                         
             ar.isLoggedIn(function(a, b) {
                 callback(a, b);
-                sinon.assert.calledWith(callback, true, res);
+                sinon.assert.calledWith(callback, true, resLogin1);
                 done();
             });
             
-            requests[0].respond(200, { "Content-Type": "application/json" }, JSON.stringify(res));
+            requests[0].respond(200, {"Content-Type": "application/json"}, JSON.stringify(resLogin1));
+        });
+        
+        test('LoginStatus = 0 from server', function(done) {
+            var callback = sinon.spy();
+                        
+            ar.isLoggedIn(function(a, b) {
+                callback(a, b);
+                sinon.assert.calledWith(callback, false, resLogin0);
+                done();
+            });
+            
+            requests[0].respond(200, {"Content-Type": "application/json"}, JSON.stringify(resLogin0));
         });
         
     });
